c8e282689b1b598d261008bef55f5ab8033bf6bf
Ishdeep2003 <166274951+Ishdeep2003@users.noreply.github.com>
2025-03-04 12:21:49 +0530

    Add files via upload

---
+++ file 1.R
@@ -0,0 +1,20 @@
+seq(from=2 , to=10, by=0.5)
+seq(from=2 , to=10, length=6)
+rep(5,3)
+rep(2:5,each=3)
+rep(-1:3, length.out=10)
+2^(0:10)
+1:3+rep(seq(from=0,by=10,to=30),each=3)
+1:10*c(-1,1)
+1:7*1:2
+1:3*1:4
+1:3*3:1
+x<-c(5,9,2,14,-2)
+x[3]
+x[c(2,3,5)]
+x[3:length(x)]
+x>4
+x<-c("Hello","How do u do","lovely to meet u",42)
+x
+c(x[1:2],"goodbye")
+matrix(1:12, nrow=3, ncol=4)
